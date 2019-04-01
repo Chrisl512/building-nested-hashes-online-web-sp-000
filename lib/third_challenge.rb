b45df7d6d8c9b54a5c14f49a96c9c00f35950442
Christopher Luna <chris@illusionlayervfx.com>
2019-04-01 01:42:29 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,17 +2,9 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => { :name => "Lord Montague", :age => "53" },
-      :matriarch => {name: "Lady Montague", age: "54"},
+      :matriarch => {},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []  
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :hero => {
-        #your key/value pairs here
-        },
-      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
